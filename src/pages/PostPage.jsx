796fe56fe295d1d42d96833e712c961cdcbb38db
obkin <kapellwork@gmail.com>
2024-01-18 17:30:19 +0200

    added: PostPage -> useFetching()

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -2,27 +2,41 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import PostsService from '../API/PostsService';
 import '../styles/PostPage.css';
+import { useFetching } from '../hooks/useFetching';
+import Loader from '../components/UI/Loader/Loader';
 
 const PostPage = () => {
-    const [post, setPost] = useState({});
     const params = useParams();
-
-    async function getPost(postId) {
-        const result = await PostsService.getPost(postId);
+    const [post, setPost] = useState({});
+    const [fetchByPostId, isLoading, error] = useFetching(async () => {
+        const result = await PostsService.getPost(params.id);
         setPost(result.data);
-    }
+    });
 
     useEffect(() => {
-        getPost(params.id);
+        fetchByPostId();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
-        <div className='post__page__wrapper'>
-            <div className='post__page__content'>
-                <h1 className='post__page__title'>post #{post.id}</h1>
-                <h2>{post.title}</h2>
-                <h2>{post.body}</h2>
+        <div>
+            {error
+                ? 
+                    <h1>Error: {error}</h1>
+                :
+                    <div className='post__page__wrapper'>
+                {isLoading
+                    ? 
+                        <Loader/>
+                    : 
+                        <div className='post__page__content'>
+                        <h1 className='post__page__title'>post #{post.id}</h1>
+                        <h2>{post.title}</h2>
+                        <h2>{post.body}</h2>
+                </div>
+                }
             </div>
+            }
         </div>
     );
 };
