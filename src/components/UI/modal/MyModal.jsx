cc5c774ea0b7b1c4dc4b08f1a2783a28c6cc9d2b
obkin <kapellwork@gmail.com>
2024-01-30 22:22:51 +0200

    added: MyModal -> closing by ESC

--- src/components/UI/modal/MyModal.jsx
+++ src/components/UI/modal/MyModal.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './MyModal.module.css';
 
 const MyModal = ({ children, visible, setVisible }) => {
 
     const rootClasses = [classes.myModal];
+
     if (visible) {
         rootClasses.push(classes.active);
     }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Escape') {
+            setVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [])
 
     return (
         <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
