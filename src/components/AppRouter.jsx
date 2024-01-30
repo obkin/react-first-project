d8f2e24b7c83b0aa321124174ce0a3b6a43c791b
obkin <kapellwork@gmail.com>
2024-01-30 13:53:47 +0200

    added: private and public routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { publicRoutes } from '../router/routes';
+import { privateRoutes, publicRoutes } from '../router/routes';
 
 const AppRouter = () => {
+    const [isUserAuthorized, setIsUserAuthorized] = useState(true);
+
     return (
+        isUserAuthorized
+        ? 
+        <Routes>
+            {privateRoutes.map(route =>
+                <Route
+                    element={route.component}
+                    path={route.path}
+                    exact={route.exact}
+                    key={route.path}
+                />
+            )}
+        </Routes>
+        : 
         <Routes>
             {publicRoutes.map(route =>
                 <Route
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,12 +6,22 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 
 export const publicRoutes = [
+    { path: '/register', component: <Register />, exact: true },
+    { path: '/login', component: <Login />, exact: true },
+    { path: '*', component: <Error />, exact: true },
+    
+    // Redirect to login
+    { path: '/', component: <Login />, exact: true },
+    { path: '/posts', component: <Login />, exact: true },
+];
+
+export const privateRoutes = [
     { path: '/', component: <Home />, exact: true },
     { path: '/register', component: <Register />, exact: true },
     { path: '/login', component: <Login />, exact: true },
     { path: '/posts', component: <Posts />, exact: true },
     { path: '/posts/:id', component: <PostPage />, exact: true },
     { path: '*', component: <Error />, exact: true },
+    // { path: '/user/account', component: <Home />, exact: true },
+    // { path: '/user/posts', component: <Home />, exact: true },
 ];
-
-export const privateRoutes = [];
