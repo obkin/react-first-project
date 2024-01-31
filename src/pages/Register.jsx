253996cc5f5f04f01906271dab382c7899063bbc
obkin <kapellwork@gmail.com>
2024-01-31 13:05:21 +0200

    added: Register.jsx -> checking for empty fields with red border

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react';
-import '../styles/Register.css';
 import { Link } from 'react-router-dom';
 import UsersService from '../API/UsersService';
 import { useFetching } from '../hooks/useFetching';
 import Loader from '../components/UI/Loader/Loader';
+import '../styles/Register.css';
 
 const Register = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [inputErrors, setInputErrors] = useState({ name: false, email: false, password: false });
     const [register, isRegisterLoading, registerError] = useFetching(async () => {
-        const response = await UsersService.createUser(name, email, password);
-        console.log(response);
+        if (name === '') {
+            setInputErrors((prevErrors) => ({ ...prevErrors, name: true }));
+        } else if (email === '') {
+            setInputErrors((prevErrors) => ({ ...prevErrors, email: true }));
+        } else if (password === '') {
+            setInputErrors((prevErrors) => ({ ...prevErrors, password: true }));
+        } else {
+            const response = await UsersService.createUser(name, email, password);
+            console.log(response);
+        }
     });
 
     const createNewUser = async () => {
@@ -24,45 +33,55 @@ const Register = () => {
             <h1>Registration</h1>
             <form>
                 <input
-                    className='register__input'
+                    className={`register__input ${inputErrors.name ? 'register__input__error' : ''}`}
                     type='text'
                     placeholder='name'
                     value={name}
-                    onChange={(e) => setName(e.target.value)}
+                    onChange={(e) => {
+                        setName(e.target.value);
+                        setInputErrors((prevErrors) => ({ ...prevErrors, name: false }));
+                    }}
                 />
                 <input
-                    className='register__input'
+                    className={`register__input ${inputErrors.email ? 'register__input__error' : ''}`}
                     type='text'
                     placeholder='email'
                     value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                        setInputErrors((prevErrors) => ({ ...prevErrors, email: false }));
+                    }}
                 />
                 <input
-                    className='register__input'
+                    className={`register__input ${inputErrors.password ? 'register__input__error' : ''}`}
                     type='password'
                     placeholder='password'
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                        setInputErrors((prevErrors) => ({ ...prevErrors, password: false }));
+                    }}
                 />
                 <div className='register__btn__wrapper'>
-                    {isRegisterLoading
-                        ? 
+                    {isRegisterLoading ? (
                         <div className='register__btn__loader'>
                             <Loader />
                         </div>
-                        : 
+                    ) : (
                         <button className='register__btn' type='button' onClick={() => {register()}}>
                             Register
                         </button>
-                    }
+                    )}
                 </div>
                 <div className='login__btn__wrapper'>
-                    {isRegisterLoading
-                        ? <></>
-                        : <Link className="register__link" to="/login">Login</Link>
-                    }
+                    {!isRegisterLoading && <Link className="register__link" to="/login">Login</Link>}
                 </div>
             </form>
+
+            <div className='register__error'>
+
+            </div>
+
         </div>
     );
 };
