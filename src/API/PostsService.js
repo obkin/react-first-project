50e15aae415ebc0dbcc4126ebef4ff9a7518f513
obkin <kapellwork@gmail.com>
2024-02-15 10:54:01 +0200

    some fixes

--- src/API/PostsService.js
+++ src/API/PostsService.js
@@ -2,16 +2,19 @@ import axios from 'axios';
 
 export default class PostsService {
 
+    // don't require additional authorization (front-end only)
     static async createPost(newPost) {
         const response = await axios.post('http://localhost:8870/posts/add-post', newPost);
         return response;
     }
 
+    // don't require authorization
     static async getPost(postId) {
         const response = await axios.get(`http://localhost:8870/posts/get-post/${postId}`);
         return response;
     }
 
+    // don't require authorization
     static async getAllPosts(limit, page) {
         const response = await axios.get('http://localhost:8870/posts/get-posts', {
             params: {
@@ -22,10 +25,11 @@ export default class PostsService {
         return response;
     }
 
+    // require additional authorization (front-end & back-end)
     static async getAllUserPosts(limit, page) {
         const response = await axios.get('http://localhost:8870/posts/get-user-posts', {
             headers: {
-                authorization: `Bearer ${localStorage.getItem("jwt")}`,
+                authorization: `Bearer ${localStorage.getItem('jwt')}`,
             },
             params: {
                 limit,
@@ -35,11 +39,13 @@ export default class PostsService {
         return response;
     }
 
+    // don't require additional authorization (front-end only)
     static async removePost(postId) {
         const response = await axios.delete(`http://localhost:8870/posts/remove-post/${postId}`);
         return response;
     }
 
+    // don't require additional authorization (front-end only)
     static async updatePost(postId, updatedPost) {
         const response = await axios.put(`http://localhost:8870/posts/update-post/${postId}`, updatedPost);
         return response;
